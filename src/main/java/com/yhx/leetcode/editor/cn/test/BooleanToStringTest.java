0206d2c5eb52a5692a75e8228bc35e126bdf6eba
yuhongxiang <yuhongxiang666@gmail.com>
2020-12-16 18:12:42 +0800

    布尔工具类

--- src/main/java/com/yhx/leetcode/editor/cn/test/BooleanToStringTest.java
+++ src/main/java/com/yhx/leetcode/editor/cn/test/BooleanToStringTest.java
@@ -1,5 +1,8 @@
 package com.yhx.leetcode.editor.cn.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A {@code BooleanToStringTest} is a test of converting String to Boolean
  * @see com.yhx.leetcode.editor.cn.Welcome#main(String[]) welcome
@@ -11,12 +14,30 @@ public class BooleanToStringTest {
         String s1 = "true";
         String s2 = "false";
         String s3 = "";
-        print(s1);
-        print(s2);
-        print(s3);
+        Integer i1 = 5;
+        print(i1);
+        i1++;
+        print(i1);
+        triple(i1);
+        print(i1);
+        Integer i2 = 5;
+        if(i1 == i2) {
+            System.out.printf("equals");
+        } else {
+            System.out.printf("not equals");
+        }
+
     }
 
-    public static void print(String str) {
-        System.out.println("str" + "--" + Boolean.parseBoolean(str));
+    public static <T> void print(T str) {
+        System.out.println("--" + str.toString());
+    }
+
+    public static void triple(Integer x){
+        x++;
+    }
+
+    public static void changeString(String str){
+        str = "change";
     }
 }
